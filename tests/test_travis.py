3686da160c07605da3b589fbb14ad187185a8af1
dvp <d.portnov@iterrf.ru>
2026-04-21 22:06:28 +0300

    test: fixin <CR>... 2

--- tests/test_travis.py
+++ tests/test_travis.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
+import sys
+import sysconfig
+
 from pathlib import Path
 import shutil
 
@@ -7,7 +10,8 @@ import pytest
 
 from numjuggler.main import main
 
-
+WIN = sys.platform.startswith("win32") and "mingw" not in sysconfig.get_platform()
+MACOS = sys.platform.startswith("darwin")
 HERE = Path(__file__).parent.absolute()
 data = HERE / "data/travis_tests"
 assert data.exists(), "Cannot access test data 'travis' files"
@@ -42,7 +46,7 @@ def test_travis(cd_tmpdir, capsys, mode, options, inp):
     subdir = data / mode
     source = subdir / (inp + ".i")
     wrk_file = shutil.copy(source, cd_tmpdir)
-    expected = _load_as_binary(subdir / (inp + ".ref"))
+    expected = load_without_cr_chars(subdir / (inp + ".ref"))
     command = ["--mode", mode, *options.split(), wrk_file]
     main(command)
     out, _ = capsys.readouterr()
@@ -58,7 +62,7 @@ def test_cdens(cd_tmpdir, capsys, inp, map_):
     source = cdense_data / (inp + ".i")
     wrk_file = shutil.copy(source, cd_tmpdir)
     wrk_map = shutil.copy(cdense_data / map_, cd_tmpdir)
-    expected = _load_as_binary(cdense_data / f"{inp}.{map_}.ref")
+    expected = load_without_cr_chars(cdense_data / f"{inp}.{map_}.ref")
     command = ["--mode", "cdens", "--map", wrk_map, wrk_file]
     main(command)
     out, _ = capsys.readouterr()
@@ -74,14 +78,17 @@ def test_merge(cd_tmpdir, capsys, inp, merged):
     inp1_path = merge_data / (inp + "1.inp")
     wrk_inp2 = shutil.copy(inp2_path, cd_tmpdir)
     wrk_inp1 = shutil.copy(inp1_path, cd_tmpdir)
-    expected = _load_as_binary(merge_data / f"{merged}.{inp}.ref")
+    expected = load_without_cr_chars(merge_data / f"{merged}.{inp}.ref")
     command = ["--mode", "merge", "-m", wrk_inp2, wrk_inp1]
     main(command)
     out, _ = capsys.readouterr()
     assert out == expected
 
 
-def _load_as_binary(path: Path) -> str:
+def load_without_cr_chars(path: Path) -> str:
     """Ensure there's no <CR> characters on Windows"""
-    with path.open("rb") as fid:
-        return fid.read().decode("utf8")
+    if WIN:
+        with path.open("rb") as fid:
+            text = fid.read().decode("utf8")
+        return text.replace("\r", "")
+    return path.read_text()
