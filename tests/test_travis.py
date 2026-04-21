ec4f904360fbeeabd7ae180205777ea61ecba1d0
dvp <d.portnov@iterrf.ru>
2026-04-21 19:07:48 +0300

    test: fix windows <CR> reading

--- tests/test_travis.py
+++ tests/test_travis.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import platform
+
 from pathlib import Path
 import shutil
 
@@ -42,11 +44,18 @@ def test_travis(cd_tmpdir, capsys, mode, options, inp):
     subdir = data / mode
     source = subdir / (inp + ".i")
     wrk_file = shutil.copy(source, cd_tmpdir)
-    expected = (subdir / (inp + ".ref")).read_text()
+    expected = fix_cr(subdir / (inp + ".ref"))
     command = ["--mode", mode, *options.split(), wrk_file]
     main(command)
     out, _ = capsys.readouterr()
     assert out == expected
+
+
+def fix_cr(path):
+    expected = path.read_text()
+    if platform.system() == "Windows":
+        expected = expected.replace("\r", "")
+    return expected
 
 
 cdense_data = Path(data / "cdens")
@@ -58,7 +67,7 @@ def test_cdens(cd_tmpdir, capsys, inp, map_):
     source = cdense_data / (inp + ".i")
     wrk_file = shutil.copy(source, cd_tmpdir)
     wrk_map = shutil.copy(cdense_data / map_, cd_tmpdir)
-    expected = (cdense_data / f"{inp}.{map_}.ref").read_text()
+    expected = fix_cr(cdense_data / f"{inp}.{map_}.ref")
     command = ["--mode", "cdens", "--map", wrk_map, wrk_file]
     main(command)
     out, _ = capsys.readouterr()
@@ -74,7 +83,7 @@ def test_merge(cd_tmpdir, capsys, inp, merged):
     inp1_path = merge_data / (inp + "1.inp")
     wrk_inp2 = shutil.copy(inp2_path, cd_tmpdir)
     wrk_inp1 = shutil.copy(inp1_path, cd_tmpdir)
-    expected = (merge_data / f"{merged}.{inp}.ref").read_text()
+    expected = fix_cr(merge_data / f"{merged}.{inp}.ref")
     command = ["--mode", "merge", "-m", wrk_inp2, wrk_inp1]
     main(command)
     out, _ = capsys.readouterr()
